2e8f2319a6ff3f3ec2fb4e4f774120bc004aa5f2
unknown <ameerhamzabutt555@gmail.com>
2023-07-04 23:27:04 +0500

    model creation and also super user created

--- management/myapp/admin.py
+++ management/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client, Expenses, Vendor, Store, Salaries
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(Expenses)
+admin.site.register(Vendor)
+admin.site.register(Store)
+admin.site.register(Salaries)
--- management/myapp/models.py
+++ management/myapp/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    client_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+
+class Vendor(models.Model):
+    vendor_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+
+
+class Store(models.Model):
+    store_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+
+
+class Salaries(models.Model):
+    salary_id = models.AutoField(primary_key=True)
+    employee = models.ForeignKey(Client, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date_paid = models.DateField()
+
+
+
+class Expenses(models.Model):
+    expense_id = models.AutoField(primary_key=True)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    description = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
