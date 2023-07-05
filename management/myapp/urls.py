02fd82eec9c993f7ae5aeeb0f3ba9505ca02e2ae
ameerhamzabutt555 <ameerhamzabutt555@gmail.com>
2023-07-05 17:52:57 +0500

    ramaning template add and also route created

--- management/myapp/urls.py
+++ management/myapp/urls.py
@@ -6,6 +6,13 @@ from .views import ClientListAPIView, ExpenessListAPIView, SalariesListAPIView, 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('client/', views.client, name='client'),
+    path('expenes/', views.expenes, name='expenes'),
+    path('salary/', views.salary, name='salary'),
+    path('store/', views.store, name='store'),
+    path('vendor/', views.vendor, name='vendor'),
+    
+    
     path('clients/', ClientListAPIView.as_view(), name='client-list'),
     path('expeness/', ExpenessListAPIView.as_view(), name='expeness-list'),
     path('salaries/', SalariesListAPIView.as_view(), name='salaries-list'),
--- management/myapp/views.py
+++ management/myapp/views.py
@@ -5,7 +5,11 @@ from django.http import JsonResponse
 from .serializers import ClientSerializer, VendorSerializer, StoreSerializer, SalariesSerializer, ExpensesSerializer
 from .models import Client, Vendor, Store, Salaries, Expenses
 
+
 def index(request):
+    return render(request, 'myapp/index.html')
+
+def client(request):
     api_url = 'http://127.0.0.1:8000/myapp/clients/'  # Replace with your API endpoint URL
     response = requests.get(api_url)
 
@@ -15,8 +19,57 @@ def index(request):
     else:
         error_message = 'API request failed'
         # Handle the error condition
-    return render(request, 'myapp/index.html',{'clients': clients_data})
+    return render(request, 'myapp/client.html',{'clients': clients_data})
 
+
+def expenes(request):
+    api_url = 'http://127.0.0.1:8000/myapp/expeness/'  # Replace with your API endpoint URL
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        expenses_data = response.json()
+        # Process the API data as needed
+    else:
+        error_message = 'API request failed'
+        # Handle the error condition
+    return render(request, 'myapp/expenses.html',{'expenses': expenses_data})
+
+def salary(request):
+    api_url = 'http://127.0.0.1:8000/myapp/salaries/'  # Replace with your API endpoint URL
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        salaries_data = response.json()
+        # Process the API data as needed
+    else:
+        error_message = 'API request failed'
+        # Handle the error condition
+    return render(request, 'myapp/salaries.html',{'salaries': salaries_data})
+
+
+def store(request):
+    api_url = 'http://127.0.0.1:8000/myapp/stores/'  # Replace with your API endpoint URL
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        stores_data = response.json()
+        # Process the API data as needed
+    else:
+        error_message = 'API request failed'
+        # Handle the error condition
+    return render(request, 'myapp/store.html',{'stores': stores_data})
+
+def vendor(request):
+    api_url = 'http://127.0.0.1:8000/myapp/vendors/'  # Replace with your API endpoint URL
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        vendor_data = response.json()
+        # Process the API data as needed
+    else:
+        error_message = 'API request failed'
+        # Handle the error condition
+    return render(request, 'myapp/vendor.html',{'vendors': vendor_data})
 
 
 class ClientListAPIView(generics.ListAPIView):
