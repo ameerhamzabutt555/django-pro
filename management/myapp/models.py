9e363123c4918b23dffbd08045b78547c05fb38c
muhammadumarjaved44 <muhammadumarjaved44@gmail.com>
2023-07-06 00:57:25 +0500

    add models

--- management/myapp/models.py
+++ management/myapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
 
 class Client(models.Model):
     client_id = models.AutoField(primary_key=True)
@@ -12,6 +14,9 @@ class Client(models.Model):
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=20)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
 
@@ -26,7 +31,11 @@ class Vendor(models.Model):
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=20)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
 
 
 class Store(models.Model):
@@ -38,9 +47,82 @@ class Store(models.Model):
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=20)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
 
+
+class StockItems(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class StockInward(models.Model):
+    id = models.AutoField(primary_key=True)
+    stock_item_id = models.ForeignKey(
+        StockItems,
+        on_delete=models.CASCADE,
+    )  # (Foreign Key to StockItems.id)
+    store_id = models.ForeignKey(
+        Store,
+        on_delete=models.CASCADE,
+    )  # (Foreign Key to Stores.id)
+    quantity = models.IntegerField(null=True)
+    vendor_id = models.ForeignKey(
+        Vendor, on_delete=models.CASCADE
+    )  # reign Key to Suppliers.id)
+    invoice_number = models.CharField(max_length=100, null=True)
+    purchase_order_number = models.CharField(max_length=100, null=True)
+    received_date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class StockOutward(models.Model):
+    id = models.AutoField(primary_key=True)
+    stock_item_id = models.ForeignKey(
+        StockItems,
+        on_delete=models.CASCADE,
+    )
+    store_id = models.ForeignKey(
+        Store,
+        on_delete=models.CASCADE,
+    )
+    quantity = models.IntegerField(null=True)
+    customer_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    recipient = models.CharField(max_length=100, null=True)
+    reason = models.TextField(null=True)
+    outward_date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class StockAdjustment(models.Model):
+    id = models.AutoField(primary_key=True)
+    stock_item_id = models.ForeignKey(
+        StockItems,
+        on_delete=models.CASCADE,
+    )
+    store_id = models.ForeignKey(
+        Store,
+        on_delete=models.CASCADE,
+    )
+    quantity = models.IntegerField(null=True)
+    reason = models.TextField(null=True)
+    adjusted_date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Salaries(models.Model):
@@ -48,7 +130,8 @@ class Salaries(models.Model):
     employee = models.ForeignKey(Client, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date_paid = models.DateField()
-
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Expenses(models.Model):
@@ -57,3 +140,5 @@ class Expenses(models.Model):
     description = models.TextField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
--- management/myapp/urls.py
+++ management/myapp/urls.py
@@ -1,21 +1,24 @@
 from django.urls import path
 from . import views
-from .views import ClientListAPIView, ExpenessListAPIView, SalariesListAPIView, StoresListAPIView, VendorsListAPIView
-
+from .views import (
+    ClientListAPIView,
+    ExpenessListAPIView,
+    SalariesListAPIView,
+    StoresListAPIView,
+    VendorsListAPIView,
+)
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('client/', views.client, name='client'),
-    path('expenes/', views.expenes, name='expenes'),
-    path('salary/', views.salary, name='salary'),
-    path('store/', views.store, name='store'),
-    path('vendor/', views.vendor, name='vendor'),
-    
-    
-    path('clients/', ClientListAPIView.as_view(), name='client-list'),
-    path('expeness/', ExpenessListAPIView.as_view(), name='expeness-list'),
-    path('salaries/', SalariesListAPIView.as_view(), name='salaries-list'),
-    path('stores/', StoresListAPIView.as_view(), name='stores-list'),
-    path('vendors/', VendorsListAPIView.as_view(), name='vendors-list'),
+    path("", views.index, name="index"),
+    path("client/", views.client, name="client"),
+    path("expenes/", views.expenes, name="expenes"),
+    path("salary/", views.salary, name="salary"),
+    path("store/", views.store, name="store"),
+    path("vendor/", views.vendor, name="vendor"),
+    path("clients/", ClientListAPIView.as_view(), name="client-list"),
+    path("expeness/", ExpenessListAPIView.as_view(), name="expeness-list"),
+    path("salaries/", SalariesListAPIView.as_view(), name="salaries-list"),
+    path("stores/", StoresListAPIView.as_view(), name="stores-list"),
+    path("vendors/", VendorsListAPIView.as_view(), name="vendors-list"),
 ]
