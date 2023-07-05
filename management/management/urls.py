8a6861f545cdc506a6ab634c74f98daa56323ec8
muhammadumarjaved44 <muhammadumarjaved44@gmail.com>
2023-07-06 00:56:10 +0500

    URL change to root

--- management/management/urls.py
+++ management/management/urls.py
@@ -20,6 +20,6 @@ from django.urls import include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("myapp.urls")),
 ]
