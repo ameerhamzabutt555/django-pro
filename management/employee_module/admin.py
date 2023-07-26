85c245eb91c7d4b847c9f25e049edadf3e14284c
ameerhamzabutt555 <ameerhamzabutt555@gmail.com>
2023-07-26 22:16:07 +0500

    saly and employee

--- management/employee_module/admin.py
+++ management/employee_module/admin.py
@@ -1,3 +1,53 @@
+# from django.contrib import admin
+# from employee_module.models import Employee, Salary
+# from import_export.admin import ExportActionMixin
+
+
+# class SalaryInline(admin.TabularInline):
+#     model = Salary
+#     extra = 0
+
+#     def get_readonly_fields(self, request, obj=None):
+#         if obj:  # Check if editing an existing employee
+#             return ["created_at", "updated_at"]
+#         else:  # Creating a new employee
+#             return []  # Allow editing all fields
+
+
+# class EmployeeAdmin(ExportActionMixin,admin.ModelAdmin):
+#     inlines = [SalaryInline]
+
+#     list_display = [
+#         "id",
+#         "user",
+#         "designation",
+#         "hire_date",
+#         "created_at",
+#         "updated_at",
+#     ]
+#     list_filter = [field.name for field in Employee._meta.fields]
+#     search_fields = list_display
+#     list_per_page = 25
+# export_formats = ('csv','xls','tsv','ods','yaml','xlsx', 'json','html')
+
+# class SalaryAdmin(ExportActionMixin,admin.ModelAdmin):
+#     list_display = [field.name for field in Salary._meta.get_fields()]
+#     list_filter = [field.name for field in Salary._meta.fields]
+#     search_fields = ['net_payable', 'employee__user__username','date']
+#     list_per_page = 25
+#     readonly_fields = ["net_payable", "created_at", "updated_at"]
+
+#     def save_model(self, request, obj, form, change):
+#         obj.net_payable = obj.employee.salary_amount
+
+#         super().save_model(request, obj, form, change)
+# export_formats = ('csv','xls','tsv','ods','yaml','xlsx', 'json','html')
+
+# admin.site.register(Employee, EmployeeAdmin)
+# admin.site.register(Salary, SalaryAdmin)
+
+
+
 from django.contrib import admin
 from employee_module.models import Employee, Salary
 from import_export.admin import ExportActionMixin
@@ -14,13 +64,13 @@ class SalaryInline(admin.TabularInline):
             return []  # Allow editing all fields
 
 
-class EmployeeAdmin(ExportActionMixin,admin.ModelAdmin):
+class EmployeeAdmin(ExportActionMixin, admin.ModelAdmin):
     inlines = [SalaryInline]
 
     list_display = [
         "id",
         "user",
-        "salary_amount",
+        "designation",
         "hire_date",
         "created_at",
         "updated_at",
@@ -28,22 +78,20 @@ class EmployeeAdmin(ExportActionMixin,admin.ModelAdmin):
     list_filter = [field.name for field in Employee._meta.fields]
     search_fields = list_display
     list_per_page = 25
-export_formats = ('csv','xls','tsv','ods','yaml','xlsx', 'json','html')
+    readonly_fields = ["created_at", "updated_at"]
 
-class SalaryAdmin(ExportActionMixin,admin.ModelAdmin):
+
+class SalaryAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = [field.name for field in Salary._meta.get_fields()]
     list_filter = [field.name for field in Salary._meta.fields]
-    search_fields = ['amount_paid', 'employee__user__username','date']
+    search_fields = ['net_payable', 'employee__user__username', 'date']
     list_per_page = 25
-    readonly_fields = ["amount_paid", "created_at", "updated_at"]
+    readonly_fields = ["employee", "working_days", "leave_taken", "month", "year", "net_payable"]
 
     def save_model(self, request, obj, form, change):
-        # Assign the monthly salary amount to the salary record
-        print("Printjfjdfdsjhj", obj.employee.salary_amount)
-        obj.amount_paid = obj.employee.salary_amount
-
+        obj.net_payable = obj.basic_salary + obj.allowance - obj.deductions
         super().save_model(request, obj, form, change)
-export_formats = ('csv','xls','tsv','ods','yaml','xlsx', 'json','html')
+
 
 admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(Salary, SalaryAdmin)
--- management/employee_module/models.py
+++ management/employee_module/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+
 class Employee(models.Model):
     id = models.AutoField(primary_key=True)
+    designation = models.CharField(max_length=100,default='Unknown')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    salary_amount = models.DecimalField(max_digits=10, decimal_places=2)
     hire_date = models.DateField(default=timezone.now)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
@@ -16,13 +17,26 @@ class Employee(models.Model):
 
 
 class Salary(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, blank=True)
-    amount_paid = models.DecimalField(
-        max_digits=8, decimal_places=2, default=None, blank=True
-    )
-    date = models.DateField(default=timezone.now)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    working_days = models.PositiveIntegerField(default=0)
+    leave_taken = models.PositiveIntegerField(default=0)
+    basic_salary = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    allowance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    deductions = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    advance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    leave_absent_deduction = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    other_deductions = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    month = models.PositiveIntegerField(default=1)
+    year = models.PositiveIntegerField(default=1)
+    net_payable = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def save(self, *args, **kwargs):
+        # Calculate the total salary based on the provided data
+        total_salary = self.basic_salary + self.allowance
+        total_deductions = self.advance + self.leave_absent_deduction + self.other_deductions
+        self.net_payable = total_salary - total_deductions
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Salary of {self.employee.user.username} - {self.amount_paid}"
+        return f"Pay Slip {self.employee.user.username} - {self.month}-{self.year}"
+
