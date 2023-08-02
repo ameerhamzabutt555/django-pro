ce09d808169fe08a61f2e5d58c9d228a46434321
muhammadumarjaved44 <muhammadumarjaved44@gmail.com>
2023-08-02 09:36:58 +0500

    calculate the salary and leave

--- management/employee_salary_module/admin.py
+++ management/employee_salary_module/admin.py
@@ -1,16 +1,125 @@
 from django.contrib import admin
-from .models import Employee, Attendance
+from django.utils.html import format_html
+from .models import Employee, Attendance, Salary
+
 
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'designation', 'department', 'hire_date', 'monthly_income')
-    list_filter = ('designation', 'department', 'hire_date')
-    search_fields = ('first_name', 'last_name')
+    list_display = (
+        "first_name",
+        "last_name",
+        "designation",
+        "department",
+        "hire_date",
+        "monthly_income",
+        "allowance",
+    )
+    list_filter = ("designation", "department", "hire_date")
+    search_fields = ("first_name", "last_name")
 
-admin.site.register(Employee, EmployeeAdmin)
 
 class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ('employee', 'date', 'working_hours', 'extra_hours','leave_type')
-    list_filter = ('employee', 'date')
-    search_fields = ('employee__first_name', 'employee__last_name')
+    list_display = ("employee", "date", "working_hours", "extra_hours", "leave_type")
+    list_filter = ("employee", "date")
+    search_fields = ("employee__first_name", "employee__last_name")
 
+
+class SalaryAdmin(admin.ModelAdmin):
+    list_display = (
+        "employee",
+        "duration",
+        "working_days",
+        "basic_salary",
+        "earned_salary",
+        "over_time",
+        "attandence",
+        "leaves",
+        "total_amount",
+        "net_payable",
+    )
+    list_filter = ("employee",)
+    search_fields = ("employee__first_name", "employee__last_name")
+
+    readonly_fields = [
+        "working_days",
+        "basic_salary",
+        "earned_salary",
+        "over_time",
+        "attandence",
+        "leaves",
+        "allowance",
+        "total_amount",
+        "net_payable",
+    ]
+
+    def save_model(self, request, obj, form, change):
+        # Calculate the fields before saving the Salary instance
+
+        # Calculate Total
+        obj.basic_salary = obj.employee.monthly_income
+        obj.allowance = obj.employee.allowance
+        obj.over_time = self.calculate_overtime(obj)
+
+        obj.working_days = self.calculate_working_days(obj)
+        obj.leaves = self.calculate_leaves(obj)
+        obj.attandence = obj.working_days - obj.leaves
+        obj.earned_salary = float(obj.working_days * (obj.basic_salary / 30))
+
+        obj.total_amount = (
+            float(obj.basic_salary) + float(obj.allowance) + float(obj.over_time)
+        )
+        obj.net_payable = (
+            float(obj.earned_salary) + float(obj.allowance) + float(obj.over_time)
+        )
+
+        super().save_model(request, obj, form, change)
+
+    def calculate_working_days(self, obj):
+        # Calculate working days based on the attendance records for the employee and duration
+        # You'll need to implement this logic based on your requirements
+        # Here's a sample implementation for the sake of demonstration
+        total_working_days = 0
+        attendances = Attendance.objects.filter(
+            employee=obj.employee,
+            date__month=obj.duration.month,
+            date__year=obj.duration.year,
+        )
+        for attendance in attendances:
+            if attendance.leave_type != Attendance.UNPAID_LEAVE:
+                total_working_days += 1
+        return total_working_days
+
+    def calculate_overtime(self, obj):
+        # Calculate overtime based on extra_hours in Attendance
+        # You'll need to implement this logic based on your requirements
+        # Here's a sample implementation for the sake of demonstration
+        total_overtime_hours = 0
+        attendances = Attendance.objects.filter(
+            employee=obj.employee,
+            date__month=obj.duration.month,
+            date__year=obj.duration.year,
+        )
+        for attendance in attendances:
+            total_overtime_hours += attendance.extra_hours
+        print("total_overtime_hours", total_overtime_hours)
+        return (float(total_overtime_hours) * float(1.5)) * float(
+            obj.employee.monthly_income / 30 / 8
+        )
+
+    def calculate_leaves(self, obj):
+        total_leaves = 0
+        attendances = Attendance.objects.filter(
+            employee=obj.employee,
+            date__month=obj.duration.month,
+            date__year=obj.duration.year,
+        )
+
+        for attendance in attendances:
+            if attendance.leave_type == Attendance.UNPAID_LEAVE:
+                total_leaves += 1
+
+        return total_leaves
+
+
+admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(Attendance, AttendanceAdmin)
+admin.site.register(Salary, SalaryAdmin)
--- management/employee_salary_module/models.py
+++ management/employee_salary_module/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -7,27 +8,47 @@ class Employee(models.Model):
     department = models.CharField(max_length=100)
     hire_date = models.DateField()
     monthly_income = models.DecimalField(max_digits=10, decimal_places=2)
+    allowance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class Attendance(models.Model):
-    PAID_LEAVE = 'Paid'
-    UNPAID_LEAVE = 'Unpaid'
-    NO_LEAVE = 'None'
+    PAID_LEAVE = "Paid"
+    UNPAID_LEAVE = "Unpaid"
+    NO_LEAVE = "None"
 
     LEAVE_CHOICES = [
-        (PAID_LEAVE, 'Paid Leave'),
-        (UNPAID_LEAVE, 'Unpaid Leave'),
-        (NO_LEAVE, 'No Leave'),
+        (PAID_LEAVE, "Paid Leave"),
+        (UNPAID_LEAVE, "Unpaid Leave"),
+        (NO_LEAVE, "No Leave"),
     ]
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     date = models.DateField()
     working_hours = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     extra_hours = models.DecimalField(max_digits=5, decimal_places=2, default=0)
-    leave_type = models.CharField(max_length=10, choices=LEAVE_CHOICES, default=NO_LEAVE)
+    leave_type = models.CharField(
+        max_length=10, choices=LEAVE_CHOICES, default=NO_LEAVE
+    )
     # Add other fields as needed (e.g., leave, late arrival, early departure, etc.)
 
     def __str__(self):
         return f"{self.employee} - {self.date}"
+
+
+class Salary(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    duration = models.DateField(default=None)
+    working_days = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    basic_salary = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    earned_salary = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    over_time = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    attandence = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    leaves = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    allowance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    net_payable = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.employee} - {self.duration}"
